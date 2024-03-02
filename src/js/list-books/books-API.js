5bb3d8f8fdd849b1340f899fee5a0aba0fc5a1b8
MShafranova <mariia.shafranova@gmail.com>
2024-03-02 15:38:38 +0100

    books api done

---
+++ src/js/list-books/books-API.js
@@ -0,0 +1,48 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://books-backend.p.goit.global';
+
+export async function getCategoryList() {
+  try {
+    const response = await axios.get(`${BASE_URL}/books/category-list`);
+    return response.data.categories;
+  } catch (error) {
+    console.error('Error fetching category list:', error);
+    throw error;
+  }
+}
+
+export async function getTopBooks() {
+  try {
+    const response = await axios.get(`${BASE_URL}/books/top-books`);
+    return response.data.books;
+  } catch (error) {
+    console.error('Error fetching top books:', error);
+    throw error;
+  }
+}
+
+export async function getBooksByCategory(selectedCategory) {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/books/category?category=${selectedCategory}`
+    );
+    return response.data.books;
+  } catch (error) {
+    console.error(
+      `Error fetching books for category ${selectedCategory}:`,
+      error
+    );
+    throw error;
+  }
+}
+
+export async function getBookInfo(bookId) {
+  try {
+    const response = await axios.get(`${BASE_URL}/books/${bookId}`);
+    return response.data.book;
+  } catch (error) {
+    console.error(`Error fetching details for book ID ${bookId}:`, error);
+    throw error;
+  }
+}
